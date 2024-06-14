2c0ed00a779781cd67e255d0e044cff9607ba74f
keshav2603 <keshavsingla2603@gmail.com>
2024-06-14 14:01:58 +0530

    setup basic route to register user

--- src/app.js
+++ src/app.js
@@ -7,10 +7,16 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials:true,
 }));
-
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true, limit:"16kb"}));
 app.use(express.static("public"));
-app.use(cookieParser());
+app.use(cookieParser())
+
+//routes import 
+import userRouter from "./routes/user.routes.js"
+
+// routes declaration
+
+app.use("/api/v1/users", userRouter)
 
 export {app}; 
